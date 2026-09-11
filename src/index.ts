f9926342779d5a2abdac770c7fb37154110efb6c
gabrieldarj9-lab <gabriel.darj9@gmail.com>
2026-09-10 22:19:14 -0300

    feat(data): expose source-aware weapon data

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ export * as Resolver from "./resolver";
 export * as Metrics from "./metrics";
 export * as Scoring from "./scoring";
 export * as Progression from "./progression";
+export * as Data from "./data";
 export * from "./api";
+export * from "./data";
 
 export * as Http from "./http";
 export * from "./http";
